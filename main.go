29cc7270290cc5c64e821c113f35372cd4c11713
rflechner <rflechner@romcyber.com>
2026-03-21 13:39:38 +0100

    feat: Add support for configurable ports and output proxy URI handling

--- main.go
+++ main.go
@@ -1,37 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"httpStackLens/http"
 	"io"
 	"log"
 	"net"
+	"net/url"
 	"os"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":3128")
+	port := flag.Int("port", 3128, "listening port")
+	outputProxyUri := flag.String("output-proxy-uri", "", "URI to output proxy information")
+	flag.Parse()
+
+	var outputProxy *url.URL
+	if len(*outputProxyUri) > 0 {
+		u, err := url.Parse(*outputProxyUri)
+		if err != nil {
+			log.Printf("Invalid output proxy URI: %v\n", err)
+		}
+		outputProxy = u
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		os.Exit(1)
 	}
-	defer listener.Close()
+	defer func(listener net.Listener) {
+		err = listener.Close()
+		if err != nil {
+			log.Printf("Warning when closing browser connection: %v\n", err.Error())
+		}
+	}(listener)
 
-	fmt.Println("Socket server started on port 3128")
+	log.Printf("Socket server started on port %v\n", *port)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection:", err)
+			log.Println("Error accepting connection:", err)
 			continue
 		}
 
-		go handleConnection(conn)
+		if outputProxy != nil {
+			fmt.Printf("Using output proxy: %s\n", outputProxy.String())
+			go useOutputTunnel(conn, outputProxy)
+		} else {
+			go processTunnelRequest(conn)
+		}
 	}
 }
 
-func handleConnection(browser net.Conn) {
-	defer browser.Close()
+func processTunnelRequest(browser net.Conn) {
+	defer func(browser net.Conn) {
+		_ = browser.Close()
+	}(browser)
 
 	clientAddr := browser.RemoteAddr().String()
 	fmt.Printf("New connection from %s\n", clientAddr)
@@ -47,14 +74,72 @@ func handleConnection(browser net.Conn) {
 	webServer, err := net.Dial("tcp", fmt.Sprintf("%s:%d", request.Connect.HostPort.Host, request.Connect.HostPort.Port))
 	if err != nil {
 		browser.Write([]byte("HTTP/1.1 502 Bad Gateway\r\n\r\n"))
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	defer webServer.Close()
 
 	browser.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	log.Printf("Connection established with %s:%d for %s\n", request.Connect.HostPort.Host, request.Connect.HostPort.Port, clientAddr)
 
 	go io.Copy(browser, webServer)
 	io.Copy(webServer, browser)
+
+	fmt.Printf("Connection closed: %s\n", clientAddr)
+}
+
+func useOutputTunnel(browser net.Conn, proxy *url.URL) {
+	defer func(browser net.Conn) {
+		_ = browser.Close()
+	}(browser)
+
+	clientAddr := browser.RemoteAddr().String()
+	fmt.Printf("New connection from %s\n", clientAddr)
+
+	request, err := http.ReadProxyRequest(browser)
+	if err != nil {
+		fmt.Printf("Error reading request from %s: %v\n", clientAddr, err)
+		return
+	}
+
+	fmt.Printf("Request received: %v \n", request)
+
+	gateway, err := net.Dial("tcp", proxy.Host)
+	if err != nil {
+		browser.Write([]byte(fmt.Sprintf("HTTP/1.1 502 Bad Gateway\r\n\r\nProxy %v not available", proxy)))
+		log.Println(err)
+		return
+	}
+	defer gateway.Close()
+
+	_, err = gateway.Write([]byte(fmt.Sprintf(
+		"CONNECT %s:%d HTTP/%d.%d\r\n",
+		request.Connect.HostPort.Host, request.Connect.HostPort.Port, request.Connect.Version.Major, request.Connect.Version.Minor)))
+
+	if err != nil {
+		log.Printf("Could not send data to %s\n", proxy)
+		return
+	}
+
+	for _, header := range request.Headers {
+		if header.Name == "Proxy-Connection" {
+			continue
+		}
+		_, err = gateway.Write([]byte(fmt.Sprintf("%s: %s\r\n", header.Name, header.Value)))
+		if err != nil {
+			log.Printf("Could not send header '%s' to %s\n", header.Name, proxy)
+			return
+		}
+	}
+
+	_, err = gateway.Write([]byte("\r\n"))
+	if err != nil {
+		log.Printf("Could not send end of request to %s\n", proxy)
+		return
+	}
+
+	go io.Copy(browser, gateway)
+	io.Copy(gateway, browser)
 
 	fmt.Printf("Connection closed: %s\n", clientAddr)
 }
