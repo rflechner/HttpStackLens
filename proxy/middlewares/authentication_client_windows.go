2d987efd561320b4f539a0d2a8ee57a5031cbf73
rflechner <rflechner@romcyber.com>
2026-03-22 20:07:16 +0100

    feat: add initial structure for AuthenticationClient in Windows proxy middleware

--- proxy/middlewares/authentication_client_windows.go
+++ proxy/middlewares/authentication_client_windows.go
@@ -1,1 +1,14 @@
 package middlewares
+
+import (
+	"fmt"
+	"httpStackLens/http/ast"
+	"net"
+)
+
+type AuthenticationClient struct {
+}
+
+func (m *AuthenticationClient) HandleProxyRequest(browser net.Conn, request ast.ProxyRequest) error {
+	return fmt.Errorf("not implemented")
+}
