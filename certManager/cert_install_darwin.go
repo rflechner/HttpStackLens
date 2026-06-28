3927263ee3167adbb9f9c39d01508da44aa47627
rflechner <romain.flechner@proton.me>
2026-06-28 18:21:15 +0200

    feat: create cert installer structure for MacOS

--- certManager/cert_install_darwin.go
+++ certManager/cert_install_darwin.go
@@ -1,3 +1,22 @@
 //go:build darwin
 
 package certManager
+
+func NewCertInstaller() CertInstaller {
+	return macOsCertInstaller{}
+}
+
+// macOsCertInstaller is used on Mac OS
+type macOsCertInstaller struct{}
+
+func (macOsCertInstaller) InstallCACert(string) error {
+	return nil
+}
+
+func (macOsCertInstaller) InstallDomainCert(string) error {
+	return nil
+}
+
+func (macOsCertInstaller) IsSupported() bool {
+	return true
+}
